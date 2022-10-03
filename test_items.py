84d9df4500b7a500b0b0608d6d8ec2c9c97fee48
a.anufriev <a.anufriev@fbs.com>
2022-10-03 04:07:31 +0300

    А все за раз

---
+++ test_items.py
@@ -0,0 +1,14 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+def test_find_add_card_button(browser):
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(3)
+    try:
+        browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        present_elem = True
+    except:
+        present_elem = False
+    assert present_elem, "No add_card button on page"
